de8bb6a1827d0b98ce8d0e47c83f28c630eab99b
Brett Ernst <nobody@example.org>
2025-09-23 23:43:57 -0700

    set container name

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run -d -h media \
+docker run -d -h torrent --name torrent \
     -v /home/media:/home/media \
     -p 9091:9091 \
     --cap-add=NET_ADMIN --device /dev/net/tun \
     --dns=1.1.1.1 \
     --restart unless-stopped \
-    media
+    torrent
